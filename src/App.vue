30759c0dfbae91ca840c83dd9612546c7a2df25e
Max Gorelov <ferretblack1@gmail.com>
2022-11-01 22:07:14 +0300

    добавлена реализация задачи с инпутом

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,17 @@
     </div>
 
     <p>Это тестовый проект!</p>
+
+    <div class="block">
+      <div class="container">
+        {{ text }}
+      </div>
+
+      <div class="wrapper">
+        <input v-model="inputText" class="input-text" />
+        <button @click="send" class="input-button">Отправить</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,10 +39,21 @@ export default {
       console.log(number.value);
     };
 
+    const text = ref("Тестовый текст");
+    const inputText = ref(null);
+
+    const send = () => {
+      text.value = inputText.value;
+      inputText.value = "";
+    };
+
     return {
       number,
       setIncrement,
       setDecrement,
+      text,
+      inputText,
+      send,
     };
   },
 };
@@ -68,5 +90,33 @@ export default {
   border: 2px solid black;
   text-align: center;
   margin-top: 10px;
+}
+
+.block {
+  width: 50%;
+}
+
+.container {
+  background: gray;
+  padding: 50px;
+  border-radius: 10px;
+  color: white;
+  text-align: center;
+}
+
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+  width: 100%;
+  margin-top: 10px;
+}
+
+.input-text {
+  width: 100%;
+}
+
+.input-button {
+  width: 50%;
 }
 </style>
